596e2f0914d51321846337a9421ffed9c0636410
deffrirs <94781619+deffrirs@users.noreply.github.com>
2022-01-29 08:04:39 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ global.APIKeys = {
 	'https://zeks.me': 'Alphabott',
 }
 
-global.owner = ['62887435047326','918156874290']
+global.owner = ['6282132805286','6282132805286']
 global.packname = setting.botname
 global.author = setting.ownername
 global.alphaVersion = [2, 2143, 3]
